dfef5d919d205483d6e0c240ad51a2d87ab1f6ca
BobLiu <bobliu@bobliu.tech>
2025-12-24 22:29:30 +0800

    chore: config tools (#6)
    * chore: config dependabot
    
    * chore: add copilot code review instructions
    
    * chore: add auto build action workflow
    
    * fix: add missing packages field to pnpm-workspace.yaml
    
    * chore: fix vite failed when host env setting to relative path

--- vite.config.ts
+++ vite.config.ts
@@ -1,44 +1,50 @@
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig, loadEnv, UserConfig } from 'vite';
 import minifyHtml from 'vite-plugin-html-minifier';
 import removeConsole from 'vite-plugin-remove-console';
 
 export default defineConfig(({ mode }) => {
+  const config: UserConfig = {
+    root: 'src',
+    base: './',
+    plugins: [minifyHtml(), mode === 'production' ? removeConsole() : null],
+    build: {
+      outDir: '../dist',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          main: 'src/main.ts',
+          tracker: 'src/tracker.html',
+        },
+        output: {
+          entryFileNames: (chunkInfo) => {
+            if (chunkInfo.name === 'main') {
+              return 'main.js';
+            }
+            return 'assets/[name]-[hash].js';
+          },
+        },
+      },
+    },
+    server: {
+      open: '/debug.html',
+      host: '127.0.0.1',
+      port: 5174,
+      strictPort: true,
+    },
+  };
+
+  try {
     const env = loadEnv(mode, process.cwd());
     const apiHost = new URL(env.VITE_API_HOST);
+    if (config.server) {
+      config.server.proxy = {
+        [apiHost.pathname]: {
+          target: apiHost.origin,
+          changeOrigin: true,
+        },
+      };
+    }
+  } catch {}
 
-    return {
-        root: 'src',
-        base: './',
-        plugins: [minifyHtml(), mode === 'production' ? removeConsole() : null],
-        build: {
-            outDir: '../dist',
-            emptyOutDir: true,
-            rollupOptions: {
-                input: {
-                    main: 'src/main.ts',
-                    tracker: 'src/tracker.html',
-                },
-                output: {
-                    entryFileNames: (chunkInfo) => {
-                        if (chunkInfo.name === 'main') {
-                            return 'main.js';
-                        }
-                        return 'assets/[name]-[hash].js';
-                    },
-                },
-            },
-        },
-        server: {
-            open: '/debug.html',
-            host: '127.0.0.1',
-            port: 5174,
-            strictPort: true,
-            proxy: {
-                [apiHost.pathname]: {
-                    target: apiHost.origin,
-                    changeOrigin: true,
-                },
-            },
-        },
-    };
+  return config;
 });
