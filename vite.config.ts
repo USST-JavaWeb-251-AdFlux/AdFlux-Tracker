c2213fb9ebf1e4b42db2771b6716dbc631bd465c
BobLiu <bobliu@bobliu.tech>
2025-12-26 19:12:35 +0800

    chore: add version file generation

--- vite.config.ts
+++ vite.config.ts
@@ -3,48 +3,72 @@ import minifyHtml from 'vite-plugin-html-minifier';
 import removeConsole from 'vite-plugin-remove-console';
 
 export default defineConfig(({ mode }) => {
-  const config: UserConfig = {
-    root: 'src',
-    base: './',
-    plugins: [minifyHtml(), mode === 'production' ? removeConsole() : null],
-    build: {
-      outDir: '../dist',
-      emptyOutDir: true,
-      rollupOptions: {
-        input: {
-          main: 'src/main.ts',
-          tracker: 'src/tracker.html',
-        },
-        output: {
-          entryFileNames: (chunkInfo) => {
-            if (chunkInfo.name === 'main') {
-              return 'main.js';
-            }
-            return 'assets/[name]-[hash].js';
-          },
-        },
-      },
-    },
-    server: {
-      open: '/debug.html',
-      host: '127.0.0.1',
-      port: 5174,
-      strictPort: true,
-    },
-  };
+    const env = loadEnv(mode, process.cwd(), '');
 
-  try {
-    const env = loadEnv(mode, process.cwd());
-    const apiHost = new URL(env.VITE_API_HOST);
-    if (config.server) {
-      config.server.proxy = {
-        [apiHost.pathname]: {
-          target: apiHost.origin,
-          changeOrigin: true,
+    const config: UserConfig = {
+        root: 'src',
+        base: './',
+        plugins: [
+            minifyHtml(),
+            mode === 'production' ? removeConsole() : null,
+            {
+                name: 'version-file',
+                configureServer(server) {
+                    server.middlewares.use((req, res, next) => {
+                        if (req.url === '/version.txt') {
+                            res.setHeader('Content-Type', 'text/plain');
+                            res.end('[Dev]');
+                            return;
+                        }
+                        next();
+                    });
+                },
+                generateBundle() {
+                    this.emitFile({
+                        type: 'asset',
+                        fileName: 'version.txt',
+                        source: env.VITE_APP_VERSION ?? '[Unknown]',
+                    });
+                },
+            },
+        ],
+        build: {
+            outDir: '../dist',
+            emptyOutDir: true,
+            rollupOptions: {
+                input: {
+                    main: 'src/main.ts',
+                    tracker: 'src/tracker.html',
+                },
+                output: {
+                    entryFileNames: (chunkInfo) => {
+                        if (chunkInfo.name === 'main') {
+                            return 'main.js';
+                        }
+                        return 'assets/[name]-[hash].js';
+                    },
+                },
+            },
         },
-      };
-    }
-  } catch {}
+        server: {
+            open: '/debug.html',
+            host: '127.0.0.1',
+            port: 5174,
+            strictPort: true,
+        },
+    };
 
-  return config;
+    try {
+        const apiHost = new URL(env.VITE_API_HOST);
+        if (config.server) {
+            config.server.proxy = {
+                [apiHost.pathname]: {
+                    target: apiHost.origin,
+                    changeOrigin: true,
+                },
+            };
+        }
+    } catch {}
+
+    return config;
 });
